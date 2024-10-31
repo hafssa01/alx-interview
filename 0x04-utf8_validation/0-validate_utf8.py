138f59bffe849060a52d496a900cba560debabb8
hafssa01 <hafssazaferr@gmail.com>
2024-10-31 20:34:02 +0100

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,48 @@
 #!/usr/bin/python3
+
 """
-UTF-8 Validation Module
+method that determines if a given data set
+represents a valid UTF-8 encoding
 """
+
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
-    
-    Parameters:
-    - data (list): List of integers representing bytes of data
-    
+    Determines if a given data set represents a valid UTF-8
+    encoding.
+    Args:
+        data (List[int]): A lsit of integers representing bytes of data
     Returns:
-    - bool: True if data is valid UTF-8, False otherwise
+        bool: True if data is a valid UTF-8 encoding, else False
     """
-    num_bytes = 0  # Number of expected continuation bytes
+
+    # Number of bytes for the current character
+    num_bytes = 0
 
     for byte in data:
-        # Mask to extract the least significant 8 bits
-        byte = byte & 0xFF
-        
+        # If the most significant bit is 0, it's a one-byte character
         if num_bytes == 0:
-            # Check how many leading 1 bits are in the first byte
-            if (byte >> 5) == 0b110:   # 2-byte character
+            if (byte >> 7) == 0:
+                num_bytes = 0
+            # If the most significant bit is 10, it's not a valid
+            # start byte
+            elif (byte >> 6) == 0b10:
+                return False
+            # For multi-byte charactersm determine number of bytes
+            elif (byte >> 5) == 0b110:
                 num_bytes = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character
+            elif (byte >> 4) == 0b1110:
                 num_bytes = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character
+            elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 7) != 0:  # 1-byte character must start with 0
+            else:
                 return False
         else:
-            # Check that byte is a continuation byte (10xxxxxx)
+            # For multi-byte characters, the following bytes must
+            # start with 10
             if (byte >> 6) != 0b10:
                 return False
-        num_bytes -= 1
+            num_bytes -= 1
 
+    # If there are remaining bytes expected for a character, it's invalid
     return num_bytes == 0
