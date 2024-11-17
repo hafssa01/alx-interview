7cd9d7dc66fc178bb2da72c4efdec991b4f09a7f
hafssa01 <hafssazaferr@gmail.com>
2024-11-17 22:44:44 +0100

    starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,20 @@
 #!/usr/bin/node
-// Prints all characters of a Star Wars movie using the ALX Star Wars API
 
-if (process.argv.length !== 3) {
-    process.exit();
-  }
-  const movieID = process.argv[2];
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-  const request = require('request');
-  
-  // Recursive function to get and print character names from a list of URLs
-  function printNextCharacter (urls) {
-    // Remove first URL from list and parse it
-    const characterURL = urls.shift();
-    if (characterURL) {
-      request(characterURL, function (error, response, body) {
-        if (!error) {
-          // Parse and print the character name from the response body
-          console.log(JSON.parse(body).name);
-          // Move to next character in the list
-          printNextCharacter(urls);
-        }
-      });
+const request = require('request');
+const util = require('util');
+const filmId = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const film = (await prequest(URL, { json: true })).body;
+    for (const url of film.characters) {
+      const character = (await prequest(url, { json: true })).body;
+      console.log(character.name);
     }
+  } catch (error) {
+    console.log(error);
   }
-  
-  // Fetches a list of character's URLs then uses a callback to print each one's name
-  function printStarWarsCharacters (url) {
-    // Get list of URLs for characters in selected film
-    request(url, function (error, response, body) {
-      if (!error) {
-        // Call recursive function to fetch and print character names from URLs list
-        printNextCharacter(JSON.parse(body).characters);
-      }
-    });
-  }
-  
-  printStarWarsCharacters(url);
+})();
